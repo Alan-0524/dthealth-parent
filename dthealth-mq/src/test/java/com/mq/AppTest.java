3e6977e013cc031a35f5f0636565288df6a8632e
Alan-0524 <119241066@163.com>
2019-08-18 22:37:35 +1200

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   dthealth-mq/src/test/java/com/mq/AppTest.java

--- dthealth-mq/src/test/java/com/mq/AppTest.java
+++ dthealth-mq/src/test/java/com/mq/AppTest.java
@@ -28,27 +28,27 @@ public class AppTest extends MessageProducer {
 //        assertTrue(true);
 //    }
 //
-    @Test
-    public void testProducer() throws InterruptedException {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "139.180.163.0:9092");
-        props.put("acks", "all");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        BodyIndex bodyIndex;
-        while (true) {
-            bodyIndex = new BodyIndex();
-            Random random = new Random();
-
-            super.send("DTS", props, "long.an.0524@gmail.com", String.valueOf(new Random().nextInt()), new ProducerResultInterface() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception e) {
-                    System.out.println(metadata.offset());
-                }
-            });
-            Thread.sleep(1000L);
-        }
-    }
+//    @Test
+//    public void testProducer() throws InterruptedException {
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", "139.180.163.0:9092");
+//        props.put("acks", "all");
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        BodyIndex bodyIndex;
+//        while (true) {
+//            bodyIndex = new BodyIndex();
+//            Random random = new Random();
+//
+//            super.send("DTS", props, "long.an.0524@gmail.com", String.valueOf(new Random().nextInt()), new ProducerResultInterface() {
+//                @Override
+//                public void onCompletion(RecordMetadata metadata, Exception e) {
+//                    System.out.println(metadata.offset());
+//                }
+//            });
+//            Thread.sleep(1000L);
+//        }
+//    }
 //
 //    @Test
 //    public void testConsumer(){
