e60e67f51d53169ca1060df9d2c793f45012f4f4
Alan-0524 <119241066@163.com>
2019-08-18 22:31:03 +1200

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   dthealth-dao/src/main/java/com/dthealth/dao/utility/RedisUtility.java

--- dthealth-dao/src/main/java/com/dthealth/dao/utility/RedisUtility.java
+++ dthealth-dao/src/main/java/com/dthealth/dao/utility/RedisUtility.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class RedisUtility {
-    @Resource
+
     private RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 
     //=============================common============================
