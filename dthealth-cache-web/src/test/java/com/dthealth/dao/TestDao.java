2abb1d995ff709166e9272a2f2d6109827d8b6bd
Alan-0524 <119241066@163.com>
2019-09-30 12:17:50 +1300

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   dthealth-cache-web/src/test/java/com/dthealth/dao/TestDao.java

--- dthealth-cache-web/src/test/java/com/dthealth/dao/TestDao.java
+++ dthealth-cache-web/src/test/java/com/dthealth/dao/TestDao.java
@@ -1,26 +1,29 @@
 package com.dthealth.dao;
 
+import com.dthealth.dao.service.RedisService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestDao {
-//    @Autowired
-//    RedisService redisService;
-//
-//    @Test
-//    public void testRedis(){
-//        System.out.println("Starting");
-//        long startTime = System.currentTimeMillis();
-//        for (int i = 0; i < 100; i++) {
-//            try {
+    @Autowired
+    RedisService redisService;
+
+    @Test
+    public void testRedis(){
+        System.out.println("Starting");
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            try {
 //                redisService.storeBodyIndex(i+"","ssssssssssssssssss",2L);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        System.out.println(System.currentTimeMillis()-startTime);
-//    }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis()-startTime);
+    }
 }
